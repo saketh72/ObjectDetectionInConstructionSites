540dcb20d522bc56c011a3303933a325384aa436
saketh72 <104899396+saketh72@users.noreply.github.com>
2022-06-09 19:57:24 +0530

    Create startmodel.py

---
+++ startmodel.py
@@ -0,0 +1,37 @@
+#Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-custom-labels-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+import boto3
+
+def start_model(project_arn, model_arn, version_name, min_inference_units):
+
+    client=boto3.client('rekognition')
+
+    try:
+        # Start the model
+        print('Starting model: ' + model_arn)
+        response=client.start_project_version(ProjectVersionArn=model_arn, MinInferenceUnits=min_inference_units)
+        # Wait for the model to be in the running state
+        project_version_running_waiter = client.get_waiter('project_version_running')
+        project_version_running_waiter.wait(ProjectArn=project_arn, VersionNames=[version_name])
+
+        #Get the running status
+        describe_response=client.describe_project_versions(ProjectArn=project_arn,
+            VersionNames=[version_name])
+        for model in describe_response['ProjectVersionDescriptions']:
+            print("Status: " + model['Status'])
+            print("Message: " + model['StatusMessage']) 
+    except Exception as e:
+        print(e)
+        
+    print('Done...')
+    
+def main():
+    project_arn='arn:aws:rekognition:us-east-1:650528715408:project/construction-object-recognition-v3/1651935590277'
+    model_arn='arn:aws:rekognition:us-east-1:650528715408:project/construction-object-recognition-v3/version/constr-obj-rek-1000_v4-v2/1651937613900'
+    min_inference_units=1 
+    version_name='constr-obj-rek-1000_v4-v2'
+    start_model(project_arn, model_arn, version_name, min_inference_units)
+
+if __name__ == "__main__":
+    main()
